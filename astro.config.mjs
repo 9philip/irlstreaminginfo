cf49159d04386e959106fc9b50ed23cfe6fd4ec4
9philip <152998714+9philip@users.noreply.github.com>
2026-02-07 22:08:24 +0000

    Add custom CSS to Starlight integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-            
+            customCss: [
+            './src/styles/custom.css',
 			title: 'IRL Streaming Info',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com' }],
 			sidebar: [
