4b01116e2daa6bca8452949ce054d8a64a7180c8
9philip <152998714+9philip@users.noreply.github.com>
2026-02-08 16:55:52 +0000

    Refactor keyword arrays in table-logic.js

--- public/scripts/table-logic.js
+++ public/scripts/table-logic.js
@@ -13,16 +13,12 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  const bad = ['None', '0', 'RTMP', 'N/A', '720p30', '720p60'];
-  const limited = ['1080p30', 'Basic', '1'];
-  const goodKeywords = ['SRTLA', '1080p60', 'Advanced'];
-
     document.querySelectorAll('td:nth-child(n+4)').forEach(cell => {
       const text = cell.innerText.trim();
       const columnIndex = cell.cellIndex + 1;
 
       const bad = ['None', '0', 'RTMP', 'N/A', '720p30', '720p60'];
-      const limited = ['1080p30', '1080@30', 'Basic', '1'];
+      const limited = ['1080p30', 'Basic', '1'];
       const goodKeywords = ['SRTLA', '1080p60', 'Advanced', 'Unlimited'];
 
       if (limited.some(key => text.includes(key))) {
